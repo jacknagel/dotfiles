0c73e86acefab50477cc366d34263022b13eb1a0
Jack Nagel <jacknagel@gmail.com>
2011-02-23 04:56:11 -0600

    Updated gpg-backup.sh

--- bin/gpg-backup.sh
+++ bin/gpg-backup.sh
@@ -3,9 +3,9 @@
 BACKUP_PATH=/Volumes/GPG\ Backup
 
 if [[ -d "$BACKUP_PATH" ]]; then
-    cp -R ~/.gnupg "${BACKUP_PATH}/gnupg_$(date +%d-%m-%y)"
-    cp -R ~/.dotfiles/gpg "${BACKUP_PATH}/gpg_$(date +%d-%m-%y)"
-    cp -R ~/.dotfiles/.passwords.gpg "${BACKUP_PATH}/passwords_$(date +%d-%m-%y).gpg"
+    cp -R ~/.gnupg "${BACKUP_PATH}/gnupg/gnupg_$(date +%d-%m-%y)"
+    cp -R ~/.dotfiles/gpg "${BACKUP_PATH}/gpg/gpg_$(date +%d-%m-%y)"
+    cp -R ~/.dotfiles/.passwords.gpg "${BACKUP_PATH}/passwords/passwords_$(date +%d-%m-%y).gpg"
 else 
     echo -e "\nBackup volume ${BACKUP_PATH}/ is missing!"
     echo -e "Please attach the volume and try again.\n"
