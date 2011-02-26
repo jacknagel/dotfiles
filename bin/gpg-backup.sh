a8e1a1e8750bcca0c847350151f9e7c94bfe1e76
Jack Nagel <jacknagel@gmail.com>
2011-02-26 17:42:51 -0600

    Add SSH backup to gpg-backup.sh

--- bin/gpg-backup.sh
+++ bin/gpg-backup.sh
@@ -6,6 +6,7 @@ if [[ -d "$BACKUP_PATH" ]]; then
     cp -R ~/.gnupg "${BACKUP_PATH}/gnupg/gnupg_$(date +%d-%m-%y)"
     cp -R ~/.dotfiles/gpg "${BACKUP_PATH}/gpg/gpg_$(date +%d-%m-%y)"
     cp -R ~/.dotfiles/.passwords.gpg "${BACKUP_PATH}/passwords/passwords_$(date +%d-%m-%y).gpg"
+    cp -$ ~/.ssh "${BACKUP_PATH}/ssh/ssh_$(date +%d-%m-%y)"
 else 
     echo -e "\nBackup volume ${BACKUP_PATH}/ is missing!"
     echo -e "Please attach the volume and try again.\n"
