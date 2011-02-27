737aa2b6bc83a130269b8ad12c5a6e93100c4b54
Jack Nagel <jacknagel@gmail.com>
2011-02-27 16:33:20 -0600

    gpg-backup now runs tar -cvzf

--- bin/gpg-backup.sh
+++ bin/gpg-backup.sh
@@ -3,11 +3,29 @@
 BACKUP_PATH=/Volumes/GPG\ Backup
 
 if [[ -d "$BACKUP_PATH" ]]; then
-    cp -R ~/.gnupg "${BACKUP_PATH}/gnupg/gnupg_$(date +%d-%m-%y)"
-    cp -R ~/.dotfiles/gpg "${BACKUP_PATH}/gpg/gpg_$(date +%d-%m-%y)"
-    cp -R ~/.dotfiles/.passwords.gpg "${BACKUP_PATH}/passwords/passwords_$(date +%d-%m-%y).gpg"
-    cp -$ ~/.ssh "${BACKUP_PATH}/ssh/ssh_$(date +%d-%m-%y)"
+    
+    pushd ~/
+    tar -cvzf "temp.tar.gz" .gnupg/
+    mv "temp.tar.gz" "${BACKUP_PATH}/gnupg/gnupg_$(date +%d-%m-%y).tar.gz"
+    popd
+    
+    pushd ~/.dotfiles
+    tar -cvzf "temp.tar.gz" gpg/ 
+    mv "temp.tar.gz" "${BACKUP_PATH}/gpg/gpg_$(date +%d-%m-%y).tar.gz"
+    popd
+
+    pushd ~/.dotfiles
+    tar -cvzf "temp.tar.gz" .passwords.gpg 
+    mv "temp.tar.gz" "${BACKUP_PATH}/passwords/passwords_$(date +%d-%m-%y).tar.gz"
+    popd
+
+    pushd ~/
+    tar -cvzf "temp.tar.gz" .ssh/ 
+    mv "temp.tar.gz" "${BACKUP_PATH}/ssh/ssh_$(date +%d-%m-%y).tar.gz"
+    popd
+
 else 
     echo -e "\nBackup volume ${BACKUP_PATH}/ is missing!"
     echo -e "Please attach the volume and try again.\n"
 fi
+
