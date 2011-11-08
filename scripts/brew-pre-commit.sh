5fa2e55ff19d66a6fe4d94828f8c9bdb24700e53
Jack Nagel <jacknagel@gmail.com>
2011-11-07 20:48:52 -0600

    Remove Homebrew git hooks
    Moved to an empty branch on my local Homebrew repo.

--- scripts/brew-pre-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git diff-index --cached --name-only --diff-filter=AM HEAD | while read file
-do
-	if echo "$file" | grep -q "Library/Formula/"
-	then brew audit --strict "./$file"
-	fi
-done
