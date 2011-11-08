1b8176c470c16e6bfe21fc28015951b709e7d9f2
Jack Nagel <jacknagel@gmail.com>
2011-11-07 20:53:05 -0600

    Style changes

--- scripts/date.sh
+++ scripts/date.sh
@@ -1,14 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
+# Extract a date from filename and use it to upate the file's mtime.
+# To finish the job, use Automator or Applescript to copy the date from
+# 'date modified' to 'date created'.
+# $ touch -mt YYYYMMDDhhmm.SS filename
 
-# extract a date from filename and put it in the 'date modified' field
-# to finish the job, use Automator or Applescript to copy the date from
-# 'date modified' to 'date created'
+suffix=JPG
 
-# command format:
-# touch -mt YYYYMMDDhhmm.SS filename
-
-SUFFIX=JPG
-
-for FILE in $(ls *.$SUFFIX); do
-    touch -mt 20${FILE:4:2}${FILE:0:4}0600.00 ${FILE}
+for file in $(ls *.$suffix)
+do
+	touch -mt 20${file:4:2}${file:0:4}0600.00 ${file}
 done
