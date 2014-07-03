06ceeae822afd05ac578179ff56213d153c4384f
Jack Nagel <jacknagel@gmail.com>
2014-07-03 09:05:48 -0500

    Enhance brew-linkage script

--- bin/brew-linkage.rb
+++ bin/brew-linkage.rb
@@ -1,23 +1,62 @@
 require "set"
 require "keg"
+require "formula"
 
 ARGV.kegs.each do |keg|
-  dylibs = Set.new
+  brewed_dylibs = Hash.new { |h, k| h[k] = Set.new }
+  system_dylibs = Set.new
+  broken_dylibs = Set.new
 
   keg.find do |file|
     if file.dylib? || file.mach_o_executable? || file.mach_o_bundle?
-      dylibs.merge file.dynamically_linked_libraries
+      file.dynamically_linked_libraries.each do |dylib|
+        begin
+          owner = Keg.for Pathname.new(dylib)
+        rescue NotAKegError
+          system_dylibs << dylib
+        rescue Errno::ENOENT
+          broken_dylibs << dylib
+        else
+          brewed_dylibs[owner.name] << dylib
+        end
+      end
     end
   end
 
-  dylibs.each do |dylib|
-    begin
-      owner = Keg.for(Pathname.new(dylib))
-    rescue NotAKegError, Errno::ENOENT
+  unless system_dylibs.empty?
+    puts "System libraries:"
+    system_dylibs.each do |dylib|
+      puts "  #{dylib}"
     end
+  end
 
-    print dylib
-    print " (#{owner.name})" if owner
-    puts
+  unless brewed_dylibs.empty?
+    puts "Homebrew libraries:"
+    brewed_dylibs.each do |name, dylibs|
+      dylibs.each do |dylib|
+        puts "  #{dylib} (#{name})"
+      end
+    end
+  end
+
+  unless broken_dylibs.empty?
+    puts "Missing libraries:"
+    broken_dylibs.each do |dylib|
+      puts "  #{dylib}"
+    end
+  end
+
+  begin
+    f = Formula[keg.name]
+  rescue FormulaUnavailableError
+    next
+  end
+
+  missing_deps = brewed_dylibs.keys - f.deps.map(&:name)
+  missing_deps -= [f.name]
+
+  unless missing_deps.empty?
+    puts "Possible missing dependencies:"
+    puts "  #{missing_deps * ", "}"
   end
 end
